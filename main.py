0e0368403f8926686e5bf33e3973cbc46cabd06f
Kredis21354 <125788986+Kredis21354@users.noreply.github.com>
2023-03-26 06:46:57 +0000

    added beautifulsoup

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import requests
+from bs4 import BeautifulSoup
+
+URL = "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
+
+data = requests.get(URL).text
+
+soup = BeautifulSoup(data, features="html.parser")
+
+print(soup.find_all("td"))
