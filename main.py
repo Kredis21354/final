eb6e7cfe35a069accc5ac0f203218c932275ff0b
Kredis21354 <125788986+Kredis21354@users.noreply.github.com>
2023-03-26 07:28:43 +0000

    added kolhoz

--- main.py
+++ main.py
@@ -9,10 +9,100 @@ soup = BeautifulSoup(data, features="html.parser")
 
 movies = soup.find_all("td", {"class":"titleColumn"})
 
-for f in movies[0:10]:
+for f in movies[0:1]:
   print(f.getText())
 
 movies = soup.find_all("td", {"class":"ratingColumn imdbRating"})
 
-for f in movies[0:10]:
+for f in movies[0:1]:
+  print(f.getText())
+
+movies = soup.find_all("td", {"class":"titleColumn"})
+
+for f in movies[1:2]:
+  print(f.getText())
+
+movies = soup.find_all("td", {"class":"ratingColumn imdbRating"})
+
+for f in movies[1:2]:
+  print(f.getText())
+
+movies = soup.find_all("td", {"class":"titleColumn"})
+
+for f in movies[2:3]:
+  print(f.getText())
+
+movies = soup.find_all("td", {"class":"ratingColumn imdbRating"})
+
+for f in movies[2:3]:
+  print(f.getText())
+
+movies = soup.find_all("td", {"class":"titleColumn"})
+
+for f in movies[3:4]:
+  print(f.getText())
+
+movies = soup.find_all("td", {"class":"ratingColumn imdbRating"})
+
+for f in movies[3:4]:
+  print(f.getText())
+
+movies = soup.find_all("td", {"class":"titleColumn"})
+
+for f in movies[4:5]:
+  print(f.getText())
+
+movies = soup.find_all("td", {"class":"ratingColumn imdbRating"})
+
+for f in movies[4:5]:
+  print(f.getText())
+
+movies = soup.find_all("td", {"class":"titleColumn"})
+
+for f in movies[5:6]:
+  print(f.getText())
+
+movies = soup.find_all("td", {"class":"ratingColumn imdbRating"})
+
+for f in movies[5:6]:
+  print(f.getText())
+
+movies = soup.find_all("td", {"class":"titleColumn"})
+
+for f in movies[6:7]:
+  print(f.getText())
+
+movies = soup.find_all("td", {"class":"ratingColumn imdbRating"})
+
+for f in movies[6:7]:
+  print(f.getText())
+
+movies = soup.find_all("td", {"class":"titleColumn"})
+
+for f in movies[7:8]:
+  print(f.getText())
+
+movies = soup.find_all("td", {"class":"ratingColumn imdbRating"})
+
+for f in movies[7:8]:
+  print(f.getText())
+
+movies = soup.find_all("td", {"class":"titleColumn"})
+
+for f in movies[8:9]:
+  print(f.getText())
+
+movies = soup.find_all("td", {"class":"ratingColumn imdbRating"})
+
+for f in movies[8:9]:
+  print(f.getText())
+
+movies = soup.find_all("td", {"class":"titleColumn"})
+
+for f in movies[9:10]:
+  print(f.getText())
+
+movies = soup.find_all("td", {"class":"ratingColumn imdbRating"})
+
+for f in movies[9:10]:
   print(f.getText())
